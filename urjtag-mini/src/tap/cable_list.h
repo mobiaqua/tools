497e094721970f1dd566b2593694726a4884d229
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2017-02-18 08:41:14 +0100

    Synced with upstream

--- urjtag-mini/src/tap/cable_list.h
+++ urjtag-mini/src/tap/cable_list.h
@@ -46,6 +46,7 @@ _URJ_CABLE(ft2232_turtelizer2)
 _URJ_CABLE(ft2232_usbjtagrs232)
 _URJ_CABLE(ft2232_usbscarab2)
 _URJ_CABLE(ft2232_usbtojtagif)
+_URJ_CABLE(ft2232_digilenths1)
 #endif
 #ifdef ENABLE_CABLE_WIGGLER
 _URJ_CABLE(minimal)
